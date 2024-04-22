577b6a2774f7a32ea111e3c2e105c239087638a0
Gökhan Akyil <akyil88@outlook.de>
2024-04-22 17:46:46 +0300

    isWeakPassword Method added

---
+++ src/main/java/com/example4/PasswordValidator4.java
@@ -0,0 +1,9 @@
+package com.example4;
+
+public class PasswordValidator4 {
+
+    public boolean isWeakPassword (String password) {
+
+        return password.equals("Passwort1") || password.equals("Aa345678");
+    }
+}
