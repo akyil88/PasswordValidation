a32bf77bb53ee5c8fbe5c2ba99fc883f4b65e630
Gökhan Akyil <akyil88@outlook.de>
2024-04-22 12:16:20 +0300

    Test function activated (green)

--- src/test/java/com/example/PasswordValidatorTest.java
+++ src/test/java/com/example/PasswordValidatorTest.java
@@ -1,4 +1,17 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
 public class PasswordValidatorTest {
+
+
+
+
+    @Test
+    public void testValidatePasswordLength(){
+
+    PasswordValidator validator = new PasswordValidator();
+
+
+    }
 }
