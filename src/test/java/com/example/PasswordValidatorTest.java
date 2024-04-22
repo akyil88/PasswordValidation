560503e8bb43be0e518707e9031eef950c9058f3
Gökhan Akyil <akyil88@outlook.de>
2024-04-22 13:05:59 +0300

    Test added

--- src/test/java/com/example/PasswordValidatorTest.java
+++ src/test/java/com/example/PasswordValidatorTest.java
@@ -1,17 +1,24 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordValidatorTest {
 
 
-
-
     @Test
-    public void testValidatePasswordLength(){
+    void testValidatePasswordLength() {
+        PasswordValidator validator = new PasswordValidator();
+        boolean result1 = validator.validatePasswordLength("12345678");
+        System.out.println("Testfall 1: Passwortlänge ist größer oder gleich 8 - Erwartet: true, Ergebnis: " + result1);
+        assertTrue(result1);
 
-    PasswordValidator validator = new PasswordValidator();
 
-
+        boolean result2 = validator.validatePasswordLength("123456");
+        System.out.println("Testfall 2: Passwortlänge ist kleiner als 8 - Erwartet: false, Ergebnis: " + result2);
+        assertFalse(result2);
     }
 }
